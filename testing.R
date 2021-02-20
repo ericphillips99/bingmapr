08d7e96aed9223e3368b30cdcdc41373018c8f42
MitchDeCock <mitchdecock@gmail.com>
2021-02-20 14:03:34 -0800

    First push

---
+++ testing.R
@@ -0,0 +1,12 @@
+# install.packages("testthat")
+
+# install.packages("usethis")
+
+# Create a new test file (Set up the infrastructure for tests)
+# Create 'tests/testthat/test-<name>.R' and open it for editing
+# Set bingmapr as working directory in RStudio prior to this
+# usethis::use_testthat()
+
+usethis::use_test("geocode.R")
+
+
